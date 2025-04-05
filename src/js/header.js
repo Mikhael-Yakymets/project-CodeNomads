4192c5af403f06aaffc4f668ea0e0523ac43400f
Mykhailo Yakymets <vskitano@gmail.com>
2025-04-06 00:51:03 +0300

    header add js, swiper

--- src/js/header.js
+++ src/js/header.js
@@ -1,1 +1,15 @@
+const menuNav = document.querySelector('.header-nav-list');
+const buttonMenuNav = document.querySelector('.header-nav-menu-btn');
+const menuLinks = document.querySelectorAll('.header-nav-list a[href^="#"]');
 
+buttonMenuNav.addEventListener('click', openNavList);
+
+function openNavList() {
+  menuNav.classList.toggle('visualy-hidden');
+}
+
+menuLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    menuNav.classList.add('visualy-hidden');
+  });
+});
