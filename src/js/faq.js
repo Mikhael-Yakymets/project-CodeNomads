617b733ff7585307faad5afa0e0c3595a44ffa9a
kapustnyak <zned003@gmail.com>
2025-04-06 19:02:33 +0200

    1

---
+++ src/js/faq.js
@@ -0,0 +1,16 @@
+import Accordion from "accordion-js";
+// import "accordion-js/dist/accordion.min.css";
+
+// new Accordion('.accordion-container');
+
+const container = document.querySelector('.accordion-container');
+const accordion = new Accordion(container, {
+    showMultiple: true,
+    onOpen: (currEl) => console.log('Open!', currEl)
+});
+ 
+const buttons = accContainer.querySelectorAll('.close-accordion-btn');
+[...buttons].map((button, idx) => {
+    button.addEventListener('click', () => accordion.close(idx));
+});
+
