bfe049fa92655b22d76654082b3e493f082cc7b3
Mykhailo Yakymets <vskitano@gmail.com>
2025-04-08 00:16:39 +0300

    add modal window3

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,15 +1,15 @@
 import Accordion from 'accordion-js';
 // import "accordion-js/dist/accordion.min.css";
 
-// new Accordion('.accordion-container');
+new Accordion('.accordion-container');
 
-const container = document.querySelector('.accordion-container');
-const accordion = new Accordion(container, {
-  showMultiple: true,
-  onOpen: currEl => console.log('Open!', currEl),
-});
+// const container = document.querySelector('.accordion-container');
+// const accordion = new Accordion(container, {
+//     showMultiple: true,
+//     onOpen: (currEl) => console.log('Open!', currEl)
+// });
 
-const buttons = accContainer.querySelectorAll('.close-accordion-btn');
-[...buttons].map((button, idx) => {
-  button.addEventListener('click', () => accordion.close(idx));
-});
+// const buttons = accContainer.querySelectorAll('.close-accordion-btn');
+// [...buttons].map((button, idx) => {
+//     button.addEventListener('click', () => accordion.close(idx));
+// });
