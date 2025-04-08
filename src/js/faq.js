a1835af3e9731af5a26de873c8e219aad9c06bde
kapustnyak <zned003@gmail.com>
2025-04-08 12:15:49 +0200

    multiply open faq

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,16 +1,12 @@
 import Accordion from "accordion-js";
 // import "accordion-js/dist/accordion.min.css";
 
-new Accordion('.accordion-container-faq');
+// new Accordion('.accordion-container-faq');
 
-// const container = document.querySelector('.accordion-container');
-// const accordion = new Accordion(container, {
-//     showMultiple: true,
-//     onOpen: (currEl) => console.log('Open!', currEl)
-// });
+const container = document.querySelector('.accordion-container-faq');
+const accordion = new Accordion(container, {
+    showMultiple: true,
+    onOpen: (currEl) => console.log('Open!', currEl)
+});
  
-// const buttons = accContainer.querySelectorAll('.close-accordion-btn');
-// [...buttons].map((button, idx) => {
-//     button.addEventListener('click', () => accordion.close(idx));
-// });
 
